f99e1690c3456c9e2c9cdda98ce84e74f8379745
Leandro Sampaio <leandro.sampaio@bee.io>
2018-03-08 20:47:13 +0100

    adding libs

--- trendz-2018/header.php
+++ trendz-2018/header.php
@@ -2,61 +2,40 @@
 <html lang="pt">
     <head>
         <meta http-equiv="Content-Type" content="text/html; <?php bloginfo('charset'); ?>" />
-        <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 
-         <?php wp_head(); ?>
-    </head>
+
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>"/>
+
+
+
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/libs/reset.css" >
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/libs/bootstrap.min.css"/>
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/libs/jquery-ui.min.css"/>
+		<script src="<?php echo get_template_directory_uri(); ?>/libs/jquery-ui.min.js" type="text/javascript"></script>
+		<script src="<?php echo get_template_directory_uri(); ?>/libs/bootstrap.min.js" type="text/javascript"></script>
+		<script src="<?php echo get_template_directory_uri(); ?>/libs/jquery-3.3.1.js" type="text/javascript"></script>
+
+
+
+
+
+		<link rel="stylesheet/less" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style/style.less" />
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/3.0.0/less.min.js" type="text/javascript"></script>
+
+
+
+
+        <?php wp_head(); ?>
+
+
+	</head>
 
     <body>
 
         <header id="header">
-            <h1>Sistema Jean Luc Picard</h1>
+
         </header><!-- #header -->
 
         <div id="wrapper" class="container">
             <div class="row">
-
-
-
-                <div class="menu">
-
-                    <div class="col-1">
-                        <a href=""></a>
-                    </div>
-                    <div class="col-1">
-                        <a href=""></a>
-                    </div>
-                    <div class="col-1">
-                        <a href=""></a>
-                    </div>
-                    <div class="col-1">
-                        <a href=""></a>
-                    </div>
-                    <div class="col-1">
-                        <a href=""></a>
-                    </div>
-                    <div class="col-1">
-                        <a href=""></a>
-                    </div>
-                    <div class="col-1">
-                        <a href=""></a>
-                    </div>
-                    <div class="col-1">
-                        <a href=""></a>
-                    </div>
-                    <div class="col-1">
-                        <a href=""></a>
-                    </div>
-                    <div class="col-1">
-                        <a href=""></a>
-                    </div>
-                    <div class="col-1">
-                        <a href=""></a>
-                    </div>
-                    <div class="col-1">
-                        <a href=""></a>
-                    </div>
-
-
-                </div>
